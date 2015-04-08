ad03567e472cb48cd14fc3796018164803c82663
Alex Altair <Alex.Altair@metaswitch.com>
2015-04-08 14:21:47 -0700

    Try passing the function.

--- cookbooks/calico/recipes/general_compute.rb
+++ cookbooks/calico/recipes/general_compute.rb
@@ -4,17 +4,15 @@ controller = search(:node, "role:controller")[0][:fqdn]
 # Find the BGP neighbors, which is everyone except ourselves.
 bgp_neighbors = search(:node, "role:compute").select { |n| n[:ipaddress] != node[:ipaddress] }
 
-# Overwrite chef's IPv6 address assignment because there's more than one to choose from.
-ruby_block "assign-bgp-ipv6-address" do
-    block do
-        addresses = node[:network][:interfaces][:eth0][:addresses]
-        global_ipv6 = addresses.select do |address|
-            address[:family] == 'inet6' && address[:scope] == 'Global'
-        end
-        node[:bgp_ip6address] = global_ipv6.keys.sort[0].to_s
+# Grab the right IPv6 address because there's more than one to choose from.
+assign_ipv6 = Proc.new do |node|
+    addresses = node[:network][:interfaces][:eth0][:addresses]
+    global_ipv6 = addresses.select do |address|
+        address[:family] == 'inet6' && address[:scope] == 'Global'
     end
-    action [:nothing]
+    global_ipv6.keys.sort[0].to_s
 end
+
 
 # Tell apt about the Calico repository server.
 template "/etc/apt/sources.list.d/calico.list" do
@@ -186,7 +184,6 @@ end
 
 package "bird" do
     action [:install]
-    notifies :run, "assign-bgp-ipv6-address", :immediately
     notifies :create, "template[/etc/bird/bird.conf]", :immediately
     notifies :create, "template[/etc/bird/bird6.conf]", :immediately
 end
@@ -212,7 +209,8 @@ template "/etc/bird/bird6.conf" do
     mode "0640"
     source "compute/bird6.conf.erb"
     variables({
-        bgp_neighbors: bgp_neighbors
+        bgp_neighbors: bgp_neighbors,
+        assign_ipv6: assign_ipv6
     })
     owner "bird"
     group "bird"
