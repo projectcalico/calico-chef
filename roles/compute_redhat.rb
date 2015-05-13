cc55428c621ac7aebecd04445701647ba18a1a20
Lancelot Robson <lancelot.robson@metaswitch.com>
2015-05-13 17:16:55 +0100

    Removed APT from the redhat runlists

--- roles/compute_redhat.rb
+++ roles/compute_redhat.rb
@@ -1,7 +1,6 @@
 name "compute_redhat"
 description "Compute nodes on Redhat"
 run_list [
-    "apt",
     "recipe[calico::general_compute_redhat]"
 ]
 
--- roles/controller_redhat.rb
+++ roles/controller_redhat.rb
@@ -1,7 +1,6 @@
 name "controller_redhat"
 description "Controller node on redhat"
 run_list [
-    "apt",
     "recipe[calico::control_redhat]"
 ]
 
