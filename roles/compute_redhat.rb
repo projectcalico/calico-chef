27e03f72958edc998316b0a860fa74a6d96db683
Lancelot Robson <lancelot.robson@metaswitch.com>
2015-05-14 14:03:58 +0100

    Added yum library

--- roles/compute_redhat.rb
+++ roles/compute_redhat.rb
@@ -1,6 +1,7 @@
 name "compute_redhat"
 description "Compute nodes on Redhat"
 run_list [
+    "yum",
     "recipe[calico::general_compute_redhat]"
 ]
 
--- roles/controller_redhat.rb
+++ roles/controller_redhat.rb
@@ -1,6 +1,7 @@
 name "controller_redhat"
 description "Controller node on redhat"
 run_list [
+    "yum",
     "recipe[calico::control_redhat]"
 ]
 
