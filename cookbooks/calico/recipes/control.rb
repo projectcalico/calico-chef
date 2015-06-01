e4b39084d8b58b956b29a9f83806d78997a5bf5f
Lancelot Robson <lancelot.robson@metaswitch.com>
2015-06-01 14:07:59 +0100

    Add new juno ipv6 option

--- cookbooks/calico/recipes/control.rb
+++ cookbooks/calico/recipes/control.rb
@@ -815,6 +815,7 @@ bash "basic-networks" do
     neutron subnet-create demo-net --name demo-subnet \
       --gateway 10.28.0.1 10.28.0.0/16
     neutron subnet-create --ip-version 6 demo-net --name demo6-subnet \
+      --ipv6-address-mode dhcpv6-stateful \
       --gateway fd5f:5d21:845:1c2e:2::1 fd5f:5d21:845:1c2e:2::/80
     EOH
     not_if "neutron net-list | grep demo-net"
